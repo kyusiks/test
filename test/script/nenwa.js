65606e45f2ce0e448305c6f7d0074f3dcc57fe80
kyusiks <42104523+kyusiks@users.noreply.github.com>
2022-09-13 17:25:01 +0900

    Update nenwa.js

--- test/script/nenwa.js
+++ test/script/nenwa.js
@@ -304,3 +304,30 @@ function fnMakeM(m) {
     }
     return m
 }
+
+let gvSeemSize = 1, gvZoomSize = 1
+function fnZoom(p) {
+    if ( p == "I" ) {
+			gvSeemSize += 0.05;
+			gvZoomSize *= 1.2;
+    } else if ( p == "O" ) {
+			gvSeemSize -= 0.05;
+			gvZoomSize /= 1.2;
+    } else {
+    	return
+    }
+
+    let browser = navigator.userAgent.toLowerCase();
+    if ( browser.indexOf("firefox") != -1 ) { //브라우저가 firefox일때
+        document.body.style.webkitTransform = 'scale('+gvSeemSize+')';  
+        document.body.style.webkitTransformOrigin = '50% 0 0'; //늘리고 줄였을때위치, 
+        document.body.style.msTransform = 'scale('+gvSeemSize+')'; 
+        document.body.style.msTransformOrigin = '50% 0 0';
+        document.body.style.transform = 'scale('+gvSeemSize+')';
+        document.body.style.transformOrigin='50% 0 0';
+        document.body.style.OTransform = 'scale('+gvSeemSize+')';
+        document.body.style.OTransformOrigin='50% 0 0';
+    } else {
+        document.body.style.zoom = gvZoomSize;
+    }
+}
