ae1d3b2fa2cd945fe1d8e3ab421492c20434e858
kyusiks <42104523+kyusiks@users.noreply.github.com>
2024-05-04 14:12:11 +0900

    Add files via upload

--- test/js/list.js
+++ test/js/list.js
@@ -1,3 +1,4 @@
 var gvList = [ { val:"", txt:"과목 선택"}
              , { val:"dap", txt:"DAP 데이터 아키텍처 전문가"}
-             , { val:"cppg", txt:"CPPG 개인정보관리사" } ]
+             , { val:"cppg", txt:"CPPG 개인정보관리사" }
+             , { val:"aihw7", txt:"금융상품퀴즈" } ]
