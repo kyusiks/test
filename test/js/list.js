ec301fe002fe34ce4f09bf4baa49c2b6b11d64b4
kyusiks <42104523+kyusiks@users.noreply.github.com>
2024-07-08 11:53:27 +0900

    Update list.js

--- test/js/list.js
+++ test/js/list.js
@@ -1,4 +1,5 @@
 var gvList = [ { val:"", txt:"과목 선택"}
              , { val:"dap", txt:"DAP 데이터 아키텍처 전문가"}
              , { val:"cppg", txt:"CPPG 개인정보관리사" }
-             , { val:"aihw7", txt:"금융상품퀴즈" } ]
+             , { val:"aihw7", txt:"금융상품퀴즈" }
+             , { val:"ismsp", txt:"ISMS-P 인증심사원" } ]
